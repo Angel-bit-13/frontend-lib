ffe2e3c7a609e2b80bda4329706d2673bb84839b
Angelin <angelinraison@gmail.com>
2025-12-11 20:25:26 +0530

    Update SignupPage.jsx

--- SignupPage.jsx
+++ SignupPage.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import API from "../api/axios";
 
 const Signup = () => {
   const navigate = useNavigate();
 
   const [form, setForm] = useState({
     name: "",
+    place:"",
+    age: "",
     email: "",
-    password: "",
+    education: "",
+    phone: "",
+    password: ""
+
   });
 
   const BACKEND_URL = "http://localhost:5000";
@@ -16,9 +22,17 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post(`${BACKEND_URL}/api/auth/signup`, form);
+      await API.post(`${BACKEND_URL}/api/auth/register`, form);
       alert("Signup successful");
-      navigate("/login");
+      const loginRes = await API.post("/api/auth/login", {
+        email: form.email,
+        password: form.password,
+      });
+
+      // 3. Store token and role
+      localStorage.setItem("token", loginRes.data.token);
+      localStorage.setItem("role", loginRes.data.role);
+      navigate("/home");
     } catch (error) {
       alert("Signup failed");
     }
@@ -41,10 +55,42 @@ const Signup = () => {
         />
 
         <input
+          type="text"
+          placeholder="Place"
+          className="w-full p-3 border rounded mb-3"
+          onChange={(e) => setForm({ ...form, place: e.target.value })}
+          required
+        />
+
+        <input
+          type="number"
+          placeholder="Age"
+          className="w-full p-3 border rounded mb-3"
+          onChange={(e) => setForm({ ...form, age: e.target.value })}
+          required
+        />
+
+        <input
           type="email"
           placeholder="Email"
           className="w-full p-3 border rounded mb-3"
           onChange={(e) => setForm({ ...form, email: e.target.value })}
+          required
+        />
+
+        <input
+          type="text"
+          placeholder="Education"
+          className="w-full p-3 border rounded mb-3"
+          onChange={(e) => setForm({ ...form, education: e.target.value })}
+          required
+        />
+
+        <input
+          type="text"
+          placeholder="Phone" 
+          className="w-full p-3 border rounded mb-3"
+          onChange={(e) => setForm({ ...form, phone: e.target.value })}
           required
         />
 
@@ -56,13 +102,13 @@ const Signup = () => {
           required
         />
 
-        <button className="w-full bg-green-600 text-white p-3 rounded mt-2">
+        <button className="w-full bg-amber-800 text-white p-3 rounded mt-2">
           Sign Up
         </button>
 
         <p className="mt-4 text-center">
           Already have an account?{" "}
-          <Link to="/login" className="text-blue-600 underline">
+          <Link to="/login" className="text-amber-600 underline">
             Login
           </Link>
         </p>

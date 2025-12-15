59ec73d6fc1fecbc1610958a52989b724a624e47
Angelin <angelinraison@gmail.com>
2025-12-15 14:48:10 +0530

    Update axios.js

--- axios.js
+++ axios.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // your backend
+  baseURL: "http://localhost:5000/api",
 });
 
-// attach token if login used
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
-  return req;
-});
+API.interceptors.request.use(
+  (req) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      req.headers.Authorization = `Bearer ${token}`;
+    }
+    return req;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
